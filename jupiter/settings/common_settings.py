9012254930992e9927a2a73552b787e803587058
omprakash1989 <oppradhan2011@gmail.com>
2016-06-21 17:18:39 +0530

    Added static folder

--- jupiter/settings/common_settings.py
+++ jupiter/settings/common_settings.py
@@ -92,8 +92,9 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "media"),
+    os.path.join(BASE_DIR, "static"),
 )
+DISABLE_COLLECTSTATIC=1
 
 MEDIA_URL = '/media/'
 
